bf23e065d0cf44e79a4ebfbc12676b255cf4d914
Yogi Gilang Ramadhan <yogigilang182@gmail.com>
2024-03-06 17:39:31 +0700

    yarn update bud.config.js yarn build

--- bud.config.js
+++ bud.config.js
@@ -23,7 +23,7 @@ export default async (app) => {
    *
    * @see {@link https://bud.js.org/reference/bud.setPublicPath}
    */
-  app.setPublicPath('/app/themes/sage/public/');
+  app.setPublicPath('/wp-content/themes/personally/public/');
 
   /**
    * Development server settings
@@ -34,7 +34,7 @@ export default async (app) => {
    */
   app
     .setUrl('http://localhost:3000')
-    .setProxyUrl('http://example.test')
+    .setProxyUrl('http://wordpress.test')
     .watch(['resources/views', 'app']);
 
   /**
